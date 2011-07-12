0393b3901b5d7973661b97a8bc0978a9afb13763
Johannes Weiß <weiss@tux4u.de>
2011-07-12 16:26:01 +0200

    collect_available_events: much better

--- scripts/collect_available_events.sh
+++ scripts/collect_available_events.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-EV_URL="http://software.intel.com/sites/products/documentation/hpc/amplifierxe/en-us/lin/ug_docs/reference/hh_toc.htm"
-PFM_DIR="$HOME/build/libpfm-4.1.0/perf_examples"
+EV_URL="http://software.intel.com/sites/products/documentation/hpc/amplifierxe/en-us/lin/ug_docs/reference/hh_index.htm"
+PFM_DIR="$HOME/libpfm-4.1.0/perf_examples"
 
 trap unrec_err ERR
 function unrec_err() {
@@ -34,18 +34,19 @@ done
 
 NOAE=0
 NOUE=0
-while read arch ev; do
-    ev=$(echo $ev | tr . : | tr a-z A-Z)
-    if "$PFM_DIR/task" -e $ev true &> /dev/null; then
+while read ev; do
+    ev=$(echo $ev | tr . :)
+    if "$PFM_DIR/task" -e "$ev" true &> /dev/null; then
         let NOAE=$NOAE+1
-        echo "$arch/$ev" >> "$AV_F"
+        echo "$ev" >> "$AV_F"
     else
         let NOUE=$NOUE+1
-        echo "$arch/$ev" >> "$NAV_F"
+        echo "$ev" >> "$NAV_F"
     fi
 done < <(
-    curl "$EV_URL" |& grep -Eio '"[a-z]+/events/[a-z0-9_.]+' |
-        sed -r 's#"([a-z]+)/events/(.*)\.html#\1 \2#g'
-)
+    curl -s "$EV_URL" | grep '<option' | \
+        sed -r 's/<option[0-9a-z _\."/=-]+>([A-Za-z0-9_\.]+)<\/option>/\1/g' | \
+        sort | uniq
+    )
 
 echo SUCCESS: $NOAE available, $NOUE unavailable
