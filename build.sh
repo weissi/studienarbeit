40991ce8e37c0b8435a68dab93ec0b55343e1950
Johannes Weiß <weiss@tux4u.de>
2011-08-04 17:56:14 +0200

    build: selective builds

--- build.sh
+++ build.sh
@@ -12,15 +12,26 @@ LDFLAGS="$LDFLAGS"
 PATH="$PATH:$HERE/build:$HOME/.cabal/bin"
 cd "$HERE"
 
+BO=""
+if [ $# -eq 1 ]; then
+    BO="$1"
+fi
+
 function build_raw() {
     COMPILER="$1"
-    BINARY="$2"
+    BUILD_ONLY="$2"
+    BINARY="$3"
     shift
     shift
-    echo "- Building '$BINARY'..."
-    $COMPILER "$@" 2>&1 | while read line; do
-        echo "    $line"
-    done
+    shift
+    if [ "$BUILD_ONLY" != "" -a "$BUILD_ONLY" != "$BINARY" ]; then
+        echo "- skipping '$BINARY' (not selected)"
+    else
+        echo "- Building '$BINARY'..."
+        $COMPILER "$@" 2>&1 | while read line; do
+            echo "    $line"
+        done
+    fi
 }
 
 function build() {
@@ -43,13 +54,13 @@ echo "done"
 
 cd build
 
-build "demo_datapoints" -o $BUILD/demo_datapoints $CFLAGS $LDFLAGS \
+build "$BO" "demo_datapoints" -o $BUILD/demo_datapoints $CFLAGS $LDFLAGS \
     -lprotobuf -lprotobuf-c -lrt \
     $HERE/libdatapoints/datapoints.c $HERE/gensrc/*.c $HERE/libmisc/*.c \
     $HERE/libdatapoints/demo_datapoints.c
 
 if checklib nidaqmxbase &> /dev/null; then
-    build "datadump" -o $BUILD/datadump $CFLAGS $LDFLAGS \
+    build "$BO" "datadump" -o $BUILD/datadump $CFLAGS $LDFLAGS \
         -lprotobuf -lprotobuf-c -lrt -lnidaqmxbase \
         -pedantic -Wall -Werror \
         -ggdb \
@@ -59,7 +70,7 @@ else
     echo '- not building datadump, NIdaqMXbase not found'
 fi
 
-build "dataexport" --std=gnu99 -o $BUILD/dataexport $CFLAGS $LDFLAGS \
+build "$BO" "dataexport" --std=gnu99 -o $BUILD/dataexport $CFLAGS $LDFLAGS \
     -lprotobuf -lprotobuf-c -lrt \
     -pedantic -Wall -Werror \
     -ggdb \
@@ -67,7 +78,7 @@ build "dataexport" --std=gnu99 -o $BUILD/dataexport $CFLAGS $LDFLAGS \
     $HERE/gensrc/*.c $HERE/libmisc/*.c
 
 if checklib pfm &> /dev/null; then
-    build "dumpcounters" -o $BUILD/dumpcounters $CFLAGS $LDFLAGS \
+    build "$BO" "dumpcounters" -o $BUILD/dumpcounters $CFLAGS $LDFLAGS \
         -lpfm -lprotobuf -lprotobuf-c -lrt \
         -pedantic -Wall -Werror \
         -ggdb \
@@ -77,13 +88,13 @@ else
     echo '- not building dumpcounters, libpfm not found'
 fi
 
-build ctrbenchmark --std=gnu99 -o $BUILD/ctrbenchmark $CFLAGS $LDFLAGS \
+build "$BO" ctrbenchmark --std=gnu99 -o $BUILD/ctrbenchmark $CFLAGS $LDFLAGS \
     -Wall -Werror -pedantic -ggdb \
     $HERE/ctrbenchmark/*.c $HERE/ctrbenchmark/benchlets/*.c
 
 if which hprotoc &> /dev/null; then
     hprotoc -d../gensrc -I../protos ../protos/hs-perf-counters.proto > /dev/null
-    build_raw ghc "BuildSLE" -Wall -o $BUILD/buildsle --make \
+    build_raw ghc "$BO" "BuildSLE" -Wall -o $BUILD/buildsle --make \
         -i$HERE/gensrc $HERE/buildsle/*.hs
 else
     echo '- not building BuildSLE (Build System of Linear Equations): haskell'
