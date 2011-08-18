99902dc6331801bef9cbdd0f1f85c557666eff64
Johannes Weiß <weiss@tux4u.de>
2011-08-18 16:34:00 +0200

    build-energy-model: more options

--- scripts/build-energy-model.sh
+++ scripts/build-energy-model.sh
@@ -10,8 +10,39 @@ function err() {
 }
 
 function usage() {
-    echo "Usage: $0 MODEL-TABLE-FILE"
+    echo "Usage: $0 [-R|-C|-H] MODEL-TABLE-FILE"
+    echo
+    echo "-R: R only"
+    echo "-C: Counters only"
+    echo "-H: Human redable"
 }
+
+VERBOSE=1
+DO_R=1
+DO_C=1
+DO_H=1
+case "$1" in
+    "-R")
+        DO_C=0
+        DO_H=0
+        VERBOSE=0
+        shift
+        ;;
+    "-C")
+        DO_R=0
+        DO_H=0
+        VERBOSE=0
+        shift
+        ;;
+    "-H")
+        DO_R=0
+        DO_C=0
+        VERBOSE=0
+        shift
+        ;;
+    *)
+        ;;
+esac
 
 test $# -eq 1 || { usage; exit 1; }
 F="$1"
@@ -19,4 +50,11 @@ F="$1"
 FORMULA=$(cat -- "$F" | \
           sed -e1d -e's/"(Intercept)" //g' -e's/^"/tabledata$/g' -e's/"//g' |\
           tr " \n" "*+" | sed -r 's/\+$//g')
-echo "model <- function(tabledata) $FORMULA"
+test $VERBOSE -eq 1 && { echo "#!/usr/bin/R"; }
+test $DO_R -eq 1    && { echo "model <- function(tabledata) $FORMULA"; }
+test $VERBOSE -eq 1 && { echo; echo "# COUNTERS IN MODEL"; }
+test $DO_C -eq 1    && { sed 1,3d "$F" | cut -d'"' -f2 | tr . :; }
+test $VERBOSE -eq 1 && { echo; echo "# HUMAN READABLE"; }
+test $DO_H -eq 1    && { cat -- "$F" | \
+    sed -e1d -e's/"(Intercept)" //g' -e's/"//g'  | \
+    awk -F'[ ]' '{ print $2, "\t", $1 }'; }
