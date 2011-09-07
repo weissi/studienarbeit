cf2ab784f2b417c45810cc489b2d53e20ec8d25f
Johannes Weiß <weiss@tux4u.de>
2011-09-07 15:36:43 +0200

    thesis-build: better :-)

--- paper/thesis/build.sh
+++ paper/thesis/build.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
 HERE=$(cd $(dirname ${BASH_SOURCE[0]}) > /dev/null && pwd -P)
-TEXOPTS="-halt-on-error"
+TEXOPTS="-output-directory build -halt-on-error"
 cd "$HERE"
+
+function try_pdflatex() {
+    set +e
+    TMP=$(mktemp)
+    pdflatex "$@" &> "$TMP"
+    if [ $? -eq 0 ]; then
+        rm -- "$TMP"
+        set -e
+        return 0
+    else
+        cat -- "$TMP"
+        rm -- "$TMP"
+        set -e
+        return 1
+    fi
+}
 
 ./clean.sh
 
 set -e
-pdflatex $TEXOPTS thesis
-bibtex thesis
-pdflatex $TEXOPTS thesis
-pdflatex $TEXOPTS thesis
+try_pdflatex $TEXOPTS thesis
+bibtex build/thesis
+pdflatex $TEXOPTS -interaction=batchmode thesis
+pdflatex $TEXOPTS -halt-on-error thesis
+cp build/thesis.pdf .
 
 echo BUILD SUCCESSFUL
-
-if pgrep -f 'evince (.+\/)?thesis.pdf' > /dev/null; then
-    echo "updating evince..."
-    evince thesis.pdf
-fi
+#
+#if pgrep -f 'evince (.+\/)?thesis.pdf' > /dev/null; then
+#    echo "updating evince..."
+#    evince thesis.pdf
+#fi
