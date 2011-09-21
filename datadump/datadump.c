5087ccd38d49ebba1792a45810f94b29d5a6cb16
Johannes Weiß <weiss@tux4u.de>
2011-09-21 15:50:33 +0200

    LICENSE

--- datadump/datadump.c
+++ datadump/datadump.c
@@ -1,3 +1,20 @@
+/*
+ *  Records data in datapoints format from a NI USB-6218
+ *  Copyright (C)2011, Johannes Weiß <weiss@tux4u.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <NIDAQmxBase.h>
 #include <stdio.h>
 #include <assert.h>
