2995c00fb9414d135021d71b5aa16fddb60ad88f
Johannes Weiß <weiss@tux4u.de>
2011-10-14 10:34:35 +0200

    thesis: better repo state

--- paper/scripts/texify-git-state.sh
+++ paper/scripts/texify-git-state.sh
@@ -3,7 +3,7 @@
 echo '\\'
 echo '{\tiny'
 echo '\hfill Repository Hashes: '
-git log --pretty='\texttt{tree=%T}, \texttt{commit=%H} \\' -n1 2>&1
+git log --pretty='\texttt{tree:\ %T}, \texttt{last commit:\ %H} \\' -n1 2>&1
 echo '\begin{verbatim}'
 git status --short 2>&1
 echo '\end{verbatim}'
